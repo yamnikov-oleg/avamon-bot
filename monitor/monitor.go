cebaae2118e83b3d1d36f4e930d5d22756f08574
Oleg Yamnikov <bgeyts667@gmail.com>
2017-06-25 23:29:38 +0500

    :truck: Move monitor.go into poller.go
    Add some API types to monitor.go

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -1,42 +1,10 @@
 package monitor
 
-import (
-	"net/http"
-	"time"
-)
-
-type Poller struct {
-	Timeout time.Duration
+type Target struct {
+	Title string
+	URL   string
 }
 
-func NewPoller() *Poller {
-	return &Poller{
-		Timeout: 3 * time.Second,
-	}
-}
-
-func (p *Poller) PollService(url string) *Status {
-	client := &http.Client{}
-	client.Timeout = p.Timeout
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return newURLParsingErrorStatus(err, 0)
-	}
-
-	reqStart := time.Now()
-	resp, err := client.Do(req)
-	reqEnd := time.Now()
-	dur := reqEnd.Sub(reqStart)
-
-	if err != nil {
-		return netErrToStatus(err, dur)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return newHTTPErrorStatus(resp, dur)
-	}
-
-	return newSuccessStatus(resp, dur)
+type TargetsGetter interface {
+	GetTargets() ([]Target, error)
 }
